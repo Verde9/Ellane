7a0600785c177878aa49b9a64da8e676b02075bf
NahShon Alexander <nahshonalexander@gmail.com>
2022-07-19 14:21:47 -0700

    Added files that needed to be added and added my sout push

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,6 @@ import org.w3c.dom.ls.LSOutput;
 public class Test {
     public static void main(String[] args) {
         System.out.println("initial test...");
+        System.out.println("Nahshons push....");
     }
 }
