9b74a31211bc0a09073b1fe7a31ef6a816071b2a
NahShon Alexander <nahshonalexander@gmail.com>
2022-07-21 13:25:22 -0700

    AB#253, AB#265 AB#266  added GsonParsing...Jackson parsing might be too diffcult, I need to work with real data now to see if works

---
+++ src/main/java/com/ellane/jsonparsing/GsonClassTest.java
@@ -0,0 +1,12 @@
+package com.ellane.jsonparsing;
+import com.google.gson.*;
+
+
+public class Gson {
+    private String ROOM;
+
+    public Gson(String ROOM) {
+        this.ROOM = ROOM;
+    }
+
+}
