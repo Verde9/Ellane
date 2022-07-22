c6ca6d9b484af0a8bfd242e1db5a55952352fcc2
chrisagoro <105442034+chrisagoro@users.noreply.github.com>
2022-07-22 10:04:56 -0700

    New updated methods AB#260, AB#262, AB#263

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,11 +1,17 @@
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Player {
     private String name;
+    private String currentRoom;
+    private String firstWord;
+    private String secondWord;
     ArrayList<String> inventory = new ArrayList<>();
+    private Array Room[] = new Array[1];
 
     Scanner in = new Scanner(System.in);
+
     // Make constructor for properties
 
     public Player(String name) {
@@ -21,9 +27,64 @@ public class Player {
         this.name = name;
      }
 
-    public String makeDecision() {
+    public void makeDecision() {
+        System.out.println("What do you want to do: ");
         String decision = in.nextLine();
-        return decision;
+        verifyDecision(decision);
+    }
+
+    private void verifyDecision(String decision) {
+        String stringArr[] = decision.split(" ", 2);
+        System.out.println(stringArr);
+        firstWord = stringArr[0].toLowerCase();
+        secondWord = stringArr[1].toLowerCase();
+        System.out.println("first Word: " + firstWord);
+        System.out.println("second Word: " + secondWord);
+
+        verifyFirstWord(firstWord);
+    }
+
+    private void verifyFirstWord(String firstWord) {
+        if (firstWord.equals("look")){
+            lookInCurrentRoom();
+        }
+
+        if(firstWord.equals("controls")) {
+            showGameControls();
+        }
+
+        if(firstWord.equals("go")) {
+            verifyRoomMovement(secondWord);
+        } else {
+            System.out.println("incorrect command. Check game control for options");
+            makeDecision();
+        }
+    }
+
+    private void lookInCurrentRoom() {
+        System.out.println("current Room Description");
+        System.out.println("Items in room are going to be provided");// replace code later
+        //Call Room details here from temporary room
+        makeDecision();
+    }
+
+    private void verifyRoomMovement(String secondWord) {
+        switch(secondWord) {
+            case "east":
+                System.out.println();
+                //verify there is an east room to move to
+                   //update currentroom property
+                   //display currentRoom description
+                   //display currentRoom items by looping over them all
+                   //call makeDecision()
+                //If not valid, throw ERROR MESSAGE
+                makeDecision();
+
+                break;
+            case "west":
+                System.out.println();
+                break;
+        }
     }
 
     public ArrayList<String> getInventory() {
@@ -34,13 +95,10 @@ public class Player {
         this.inventory = inventory;
     }
 
-    public String displayInformationForLook(){
-        String input = in.nextLine();
-        String look = null;
-        if (input == "look");
-        seeItems();
-    }
-
-    private void seeItems() {
+    private void showGameControls() {
+        System.out.println("The game commands are as follows: ");
+        System.out.println("valid action commands: LOOK, USE, GO, JUMP, DROP, PICKUP, CONTROLS");
+        System.out.println("eg. 'LOOK UP', 'PICKUP SWORD', JUMP DOWN, MOVE, ");
+        makeDecision();
     }
 }
