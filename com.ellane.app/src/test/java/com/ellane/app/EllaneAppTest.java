06549d52c8c8d8ee8c23e437649925e69e3d2cb3
Lesly <brissonlesly@gmail.com>
2022-08-01 19:41:08 -0400

    small changes

--- com.ellane.app/src/test/java/com/ellane/app/EllaneAppTest.java
+++ com.ellane.app/src/test/java/com/ellane/app/EllaneAppTest.java
@@ -3,8 +3,6 @@ package com.ellane.app;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class EllaneAppTest {
