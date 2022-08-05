c8fa56b6a227e3f95d6cb2e9352e7ff79c8e6417
Verde9 <gcg145@gmail.com>
2022-08-05 16:26:47 -0600

    AB#978 Refactored old code.

--- src/com/ellane/app/EllaneApp.java
+++ src/com/ellane/app/EllaneApp.java
@@ -1,56 +1,32 @@
 package com.ellane.app;
 
-import com.ellane.model.*;
+import com.ellane.model.Locations;
+import com.ellane.model.Player;
 import com.ellane.view.EllaneView;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
 import javax.sound.sampled.*;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class EllaneApp {
     private String firstWord;
     private String secondWord;
     private Locations currentRoom;
     Boolean gameOver = false;
-    ArrayList<String> inventory = new ArrayList<>(3);
-    Boolean ellaneFound = false;
-    Boolean ellaneCured = false;
-    String ellaneLocation = "";
-    String bombLocation = "";
-    Boolean bombFound = false;
-    List<Items> gameItems = new ArrayList<>();
-    List<LocationsAndDirections> playerLocations = new ArrayList<>();
     List<Locations> location;
-    Map<String, String> playerlocations3;
-    Map<String, String> generateBasementLocation;
-    Map<String, String> generateCommon_Area_Location;
-    Map<String, String> generateLobbyLocation;
-    Map<String, String> generateMechanicalRoomLocation;
-    Map<String, String> generateOffice_1_Location;
-    Map<String, String> generateOffice_Floor_1_location;
-    Map<String, String> generateOffice_Floor_2_location;
-    Map<String, String> generateOffice_Floor_3_location;
-    Map<String, String> generateOffice_Floor_4_location;
-    Map<String, String> generateRooftop_location;
 
 
-    Player player1;
+    Player player = new Player();
     private static EllaneView view = new EllaneView();
     Scanner scan = new Scanner(System.in);
-    Player player = new Player("LB", com.ellane.model.Characters.MALE_SOLDIER);
 
 
     //this will run the app in the main class
@@ -61,49 +37,17 @@ public class EllaneApp {
     }
 
     public void checkEndGameConditions() throws NullPointerException {
-        if (player1.getHealth() <= 0 || firstWord.equals("quit") || gameOver == true) {
+        if (player.getHealth() <= 0 || firstWord.equals("quit") || gameOver == true) {
             gameOver = true;
-            view.renderEndGameMessageAndResults(player1.getHealth());
+            view.renderEndGameMessageAndResults(player.getHealth());
             System.exit(0);
-        }
-    }
-
-
-    public void checkIfEllaneIsHere() {
-        if (currentRoom.equals(ellaneLocation)) {
-            ellaneFound = true;
-            view.renderFoundEllaneLocationMessage(ellaneLocation);
-        }
-    }
-
-
-    public void checkForBombInRoom() {
-        bombFound = true;
-        if (currentRoom.equals(bombLocation)) {
-            bombFound = true;
-            gameOver = true;
-            view.renderBombFoundExplosionMessage();
         }
     }
 
     private void promptToStartGame() throws InterruptedException, IOException {
         introMusic("src/Music/For Work 2_1.wav");
-        view.renderPromptToPlayOrQuitGame();
-        String userInput = scan.nextLine().toLowerCase();
-        if (userInput.equals("quit game")) {
-            gameOver = true;
-            view.renderImmediateQuitGameMessage();
-            System.exit(0);
-        }
-        if (userInput.equals("yes")) {
-            System.out.println();
-            view.renderBeginningPlayGameMessage();
-            createPlayerOneCharacter();
-        } else {
-            view.renderInvalidCommandMessage();
-            System.out.println();
-            promptToStartGame();
-        }
+        view.renderBeginningPlayGameMessage();
+        createPlayerOneCharacter();
     }
 
 
@@ -134,78 +78,51 @@ public class EllaneApp {
             playerName = scan.nextLine();
             if (playerName.length() <= 0) {
                 view.renderEnterNameEnforceMessage();
+            } else {
+                player.setName(playerName);
             }
         }
-        System.out.println();
-        while (!valid) {
-            view.renderSelectCharacterMessage();
-            for (Characters character : Characters.values()) {
-                System.out.println(character.getCharacterType());
-            }
-            System.out.println();
-            String characterChoice = scan.nextLine().toLowerCase();
-            for (Characters character : Characters.values()) {
-                if (characterChoice.equals(character.getCharacterType())) {
-                    System.out.println();
-                    player1 = new Player(playerName, character);
-                    view.renderPlayerCreatedMessage();
-                    view.renderPlayerNameBeginGameMessage(player1.getName());
-                    startGame();
-                    valid = true;
-                    break;
-                }
-            }
-            if (!valid) {
-                System.out.println();
-                view.renderInvalidCharacterSelectedMessage();
-            }
-        }
+        startGame();
     }
 
     private void startGame() throws InterruptedException, IOException {
         view.renderDisplayGameInfo();
         generateLocation();
-        checkIfEllaneIsHere();
-        checkForBombInRoom();
         while (!gameOver) {
             promptPlayerForDecision();
         }
-        view.renderEndGameMessageAndResults(player1.getHealth());
+        view.renderEndGameMessageAndResults(player.getHealth());
     }
 
     private void promptPlayerForDecision() throws InterruptedException {
-        String decision = player1.makeDecision();
-        verifyDecision(decision);
-    }
+        System.out.println("What should you do?");
 
-    //TODO: (delete this comment later),this is making a String Array of our words so like ["john", "doe"]
-    private void verifyDecision(String decision) throws InterruptedException {
+        Scanner in = new Scanner(System.in);
+        System.out.println();
+        TimeUnit.SECONDS.sleep(1);
+        String decision = in.nextLine();
+
         String[] stringArr = decision.split(" ");
         firstWord = stringArr[0].toLowerCase();
-        try {
-            if (stringArr.length < 1) {
-                view.renderCommandIsRequiredMessage();
-                promptPlayerForDecision();
-            } else if (stringArr.length == 1) {
-                verifyFirstWord(firstWord);
-            } else {
-                secondWord = stringArr[1].toLowerCase();
-                verifyFirstWord(firstWord);
-            }
-        } catch (Exception e) {
-            view.renderInvalidCommandMessage();
+
+        if (stringArr.length == 1) {
+            verifyFirstWord(firstWord);
+        } else {
+            secondWord = stringArr[1].toLowerCase();
+            verifyFirstWord(firstWord);
         }
+
     }
 
-    //TODO: MAIN GAME LOGIC
-    private void verifyFirstWord(String firstWord) throws InterruptedException, IOException {
+
+    private void verifyFirstWord(String firstWord) throws InterruptedException {
 
         switch (firstWord) {
             case "look":
-                System.out.println("You are in " + currentRoom.getName()+ "\nYour inventory is: " + getInventory());
+                System.out.println("You are in " + currentRoom.getName() + "\nYour inventory is: " + player.getInventory());
                 System.out.println();
-                player1.decreaseHealth(2);
-                view.renderRemainingPlayerHealth(player1.getHealth());
+                player.decreaseHealth(2);
+                view.renderRemainingPlayerHealth(player.getHealth());
                 promptPlayerForDecision();
                 break;
             case "help":
@@ -217,89 +134,36 @@ public class EllaneApp {
                 System.out.println();
                 verifyRoomMovement();
                 System.out.println();
-                player1.decreaseHealth(2);
-                view.renderRemainingPlayerHealth(player1.getHealth());
+                player.decreaseHealth(2);
+                view.renderRemainingPlayerHealth(player.getHealth());
                 checkEndGameConditions();
                 promptPlayerForDecision();
                 break;
             case "inventory":
-                System.out.println("Your inventory is: " + getInventory());
+                System.out.println("Your inventory is: " + player.getInventory());
                 break;
             case "grab":
             case "get":
             case "take":
             case "pick":
-                for (LocationsAndDirections items: playerLocations){
-                    verifyRoomMovement();
-                    if (items.getItem().equals(secondWord)) {
-                        inventory.add(items.getItem());
-                        System.out.println(items.getItem_status());
-                        System.out.println();
-                        break;
-                    }
-                    if (items.getItem2().equals(secondWord)) {
-                        inventory.add(items.getItem2());
-                        System.out.println(items.getItem_status2());
-                        System.out.println();
-                        break;
-                    }
-                    else {
-                        System.out.println();
-                        System.out.println("you should probably check your inventory...");
-                    }
-                }
-                player1.decreaseHealth(2);
-                view.renderRemainingPlayerHealth(player1.getHealth());
-                checkEndGameConditions();
-                promptPlayerForDecision();
-                break;
+                // TODO: way to pick up items.
             case "drop":
-                if (inventory.contains(secondWord)) {
-                    inventory.remove(secondWord);
+                if (player.getInventory().contains(secondWord)) {
+                    player.getInventory().remove(secondWord);
                     System.out.println("you have removed this item");
                 }
                 System.out.println();
-                player1.decreaseHealth(2);
-                view.renderRemainingPlayerHealth(player1.getHealth());
+                player.decreaseHealth(2);
+                view.renderRemainingPlayerHealth(player.getHealth());
                 checkEndGameConditions();
                 promptPlayerForDecision();
                 break;
             case "health":
-                System.out.println("Your current health is: " + player1.getHealth());
+                System.out.println("Your current health is: " + player.getHealth());
                 System.out.println();
                 break;
             case "use":
-                if (inventory.contains("bazooka")) {
-                    if (generateBasementLocation.containsKey("item2") && generateBasementLocation.containsValue("bazooka")) {
-                        System.out.println("you used the bazooka the BLOW THE PLACE DOWWWWWWWN");
-                        int Min = 1;
-                        int Max = 20;
-                        int newHealth = (int) (Math.random() * ((Max - Min) + 1));
-                        player.decreaseHealth(newHealth);
-                        System.out.println("You Fucking hit yourself dummy... the player health is now "
-                                + newHealth);
-                        if (newHealth >= 10) {
-                            System.out.println("its dangerous to use the damn bazooka...");
-                        }
-                        if (newHealth < 10 && newHealth > 1) {
-                            System.out.println("Im never doing that stupid shit again");
-                        }
-                        if (newHealth <= 0) {
-                            System.out.println("...AND you fucking died.... using the bazooka you fucking idiot," +
-                                    "damn that sucks");
-                            System.out.println();
-                            gameOver = true;
-                        }
-                    }
-                } else {
-                    System.out.println("The world aint ready for that");
-                }
-                System.out.println();
-                player1.decreaseHealth(2);
-                view.renderRemainingPlayerHealth(player1.getHealth());
-                checkEndGameConditions();
-                promptPlayerForDecision();
-                break;
+                // TODO: way to use items.
             case "quit":
                 checkEndGameConditions();
                 break;
@@ -319,7 +183,7 @@ public class EllaneApp {
         }
     }
 
-    private void verifyRoomMovement() throws InterruptedException, IOException {
+    private void verifyRoomMovement() throws InterruptedException {
         String decision;
 
         if (firstWord.equals("go")) {
@@ -333,8 +197,6 @@ public class EllaneApp {
                     break;
                 default:
                     System.out.println("You can't go that way.");
-                    decision = player1.makeDecision();
-                    verifyDecision(decision);
                     break;
             }
         }
@@ -348,8 +210,6 @@ public class EllaneApp {
                     break;
                 default:
                     System.out.println("You can't do that.");
-                    decision = player1.makeDecision();
-                    verifyDecision(decision);
                     break;
             }
         }
@@ -359,19 +219,20 @@ public class EllaneApp {
 
         switch (secondWord) {
             case "north":
-              if(currentRoom.getNorth().equals(null)) {
-                  System.out.println("You can't go that way.");
-              } else {
-                  for (Locations room : location) {
-                      if (room.getName().equals(currentRoom.getNorth())) {
-                          currentRoom = room;
-                          System.out.println("You are now in the " + currentRoom.getName());
-                      }
-                  }
-              }
-            break;
+                if (currentRoom.getNorth().equals("N/A")) {
+                    System.out.println("You can't go that way.");
+                } else {
+                    for (Locations room : location) {
+                        if (room.getName().equals(currentRoom.getNorth())) {
+                            currentRoom = room;
+                            System.out.println("You are now in the " + currentRoom.getName());
+                        }
+                    }
+
+                }
+                break;
             case "south":
-                if(currentRoom.getSouth().equals(null)) {
+                if (currentRoom.getSouth().equals("N/A")) {
                     System.out.println("You can't go that way.");
                 } else {
                     for (Locations room : location) {
@@ -381,9 +242,9 @@ public class EllaneApp {
                         }
                     }
                 }
-            break;
+                break;
             case "east":
-                if(currentRoom.getEast().equals(null)) {
+                if (currentRoom.getEast().equals("N/A")) {
                     System.out.println("You can't go that way.");
                 } else {
                     for (Locations room : location) {
@@ -393,9 +254,9 @@ public class EllaneApp {
                         }
                     }
                 }
-            break;
+                break;
             case "west":
-                if(currentRoom.getWest().equals(null)) {
+                if (currentRoom.getWest().equals("N/A")) {
                     System.out.println("You can't go that way.");
                 } else {
                     for (Locations room : location) {
@@ -510,10 +371,6 @@ public class EllaneApp {
         }
 
 
-    }
-
-    public ArrayList<String> getInventory () {
-        return inventory;
     }
 
     public Boolean getGameOver () {
--- src/com/ellane/model/Player.java
+++ src/com/ellane/model/Player.java
@@ -7,19 +7,17 @@ import java.util.concurrent.TimeUnit;
 public class Player {
     private String name;
     private Integer health = 100;
-    private com.ellane.model.Characters CHARACTERTYPE;
     ArrayList<String> inventory = new ArrayList<>();
-    private LocationsAndDirections locationsAndDirections;
-
-    Scanner in = new Scanner(System.in);
 
     // Make constructor for properties
-    public Player(String name,  Characters character) {
-        setName(name);
-        setCharacterType(character);
+
+    public Player() {
+
     }
 
-
+    public Player(String name) {
+        this.name = name;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -34,31 +32,8 @@ public class Player {
         return health;
     }
 
-    public void setCharacterType(Characters CHARACTERS) {
-        this.CHARACTERTYPE = CHARACTERS;
-    }
-
-    public Characters getCharacterType() {
-        return CHARACTERTYPE;
-    }
-
     public ArrayList<String> getInventory() {
         return inventory;
-    }
-
-     //--------------METHODS------------------//
-    public String makeDecision() throws InterruptedException {
-        System.out.println("What should you do?");
-
-        Scanner in = new Scanner(System.in);
-        System.out.println();
-        TimeUnit.SECONDS.sleep(1);
-        String decision = in.nextLine();
-        return  decision;
-    }
-
-    public void dropItemFromInventory() {
-
     }
 
     public void decreaseHealth(int decreaseAmount) {
