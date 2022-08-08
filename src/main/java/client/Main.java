b0c748bb1b80026166a984f76197ff39532a5280
Andy Vargas <94484667+andyvn1@users.noreply.github.com>
2022-08-08 14:41:38 -0700

    AB#983 Re-arrange package structure t4o work with Maven

---
+++ src/main/java/client/Main.java
@@ -0,0 +1,18 @@
+package client;
+
+import app.EllaneApp;
+
+import java.io.IOException;
+
+
+public class Main {
+        public static void main(String[] args) throws InterruptedException, IOException {
+
+            EllaneApp app = new EllaneApp();
+            app.initialize();
+            //app.verifyLocation();
+            //TODO: move logic to SRC
+
+            //TODO: need to test all items...//main shouldn't throw exceptions
+        }
+    }
